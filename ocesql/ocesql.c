9cfcb9c6c3b63d87051914ccb187dd0a875a5dcd
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-09-29 19:31:06 +0900

    [Update]: replace 1.1.0 with 1.1.1

--- ocesql/ocesql.c
+++ ocesql/ocesql.c
@@ -274,7 +274,7 @@ char *cb_get_env(char *filename, int num) {
 
 void version(void) {
   printf("Open Cobol ESQL 4J\n");
-  printf("Version 1.1.0\n");
+  printf("Version 1.1.1\n");
   printf("\n");
   printf("Tokyo System House Co., Ltd. <opencobol@tsh-world.co.jp>\n");
 }
